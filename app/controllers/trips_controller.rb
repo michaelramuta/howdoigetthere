d7e6c87726c73e9b9fbd61d0e49d15c296f2a310
Michael Ramuta <michaelramuta@gmail.com>
2017-10-29 12:45:17 -0500

    add time modifiers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,6 +5,7 @@ class TripsController < ApplicationController
 
 	def index
 		trip = Trip.new()
+		trip.add_modifiers
 		json_response(trip.transport_mode)
 	end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,7 +10,48 @@ class Trip < ApplicationRecord
 	    @stats = { temp: nil, distance: nil, time: nil, trip_time: nil, price: nil, rain: nil}
 	end
 
+	def add_modifiers
+		time_modifier
+		temp_modifier
+	end
+
 	def transport_mode
 		@transports.sort_by{|k,v| v }.last[0]
+	end
+
+	def modify_stat(key, modifier)
+		@transports[key] *= modifier
+	end
+
+	def time_modifier
+		time = Time.now.hour
+		@stats[:time] = time
+		if time.between?(0, 5)
+			modify_stat(:walking, 0.8)
+			modify_stat(:bike, 0.2)
+			modify_stat(:rideshare, 1.6)
+		elsif time.between?(5, 9)
+			modify_stat(:walking, 1.4)
+			modify_stat(:bike, 1.2)
+			modify_stat(:cta, 0.8)
+		elsif time.between?(9, 13)
+			modify_stat(:walking, 1.4)
+			modify_stat(:bike, 1.4)
+		elsif time.between?(17, 19)
+			modify_stat(:walking, 1.4)
+			modify_stat(:bike, 1.4)
+			modify_stat(:cta, 0.8)
+		elsif time.between?(19, 22)
+			modify_stat(:walking, 1.4)
+			modify_stat(:rideshare, 1.1)
+			modify_stat(:cta, 1.4)
+		elsif time.between?(22, 23)
+			modify_stat(:walking, 0.8)
+			modify_stat(:bike, 0.2)
+			modify_stat(:rideshare, 1.6)
+		end
+	end
+
+	def temp_modifier
 	end
 end
